9efd16dc03c65ed7b95d45730b37378f9249eed2
Ersin Buckley <ersin@mediasuite.co.nz>
2016-10-14 14:04:08 +0800

    use pods yo

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,8 @@
-/*jshint node:true*/
-/* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
+    hinting: false
     // Add options here
   })
 
